498ede2904836277a6ff2f247eee7d8faa561ca8
Parth Patel <pp542004@gmail.com>
2024-12-13 15:01:25 +0530

    configure deploy for vercal

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vite";
+import { reactRouter } from '@react-router/dev/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
-   css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
-});
+export default defineConfig(({ isSsrBuild ,command}) => ({
+	plugins: [reactRouter(), tsconfigPaths()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss(), autoprefixer()],
+		},
+	},
+	build: {
+		rollupOptions: isSsrBuild
+			? {
+					input: './server/app.ts',
+				}
+			: undefined,
+	},
+	ssr:{
+		 noExternal: command === "build" ? true : undefined,
+	}
+}));
